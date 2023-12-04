bac0f888a672171bff6fc78c3f15d2b6607a7a41
root <root@h3c-r5300g5-s2.localdomain>
2023-12-04 19:17:33 +0800

    1

--- scripts/SMD.sh
+++
@@ -1,4 +0,0 @@
-export CUDA_VISIBLE_DEVICES=4
-
-python main.py --anomaly_ratio 0.6 --num_epochs 100   --batch_size 256  --mode train --dataset SMD  --data_path dataset/kdd/dataset/SMD   --input_c 38 --output_c 38
-# python main.py --anomaly_ratio 0.6 --num_epochs 100   --batch_size 256     --mode test    --dataset SMD   --data_path dataset/SMD     --input_c 38     --pretrained_model 20
